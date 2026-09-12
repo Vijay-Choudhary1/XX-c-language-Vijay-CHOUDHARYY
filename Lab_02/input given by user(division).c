059922d5769983b8ceb3181493e6a86d1e1d6f84
Vijay-Choudhary1 <vc587587@gmail.com>
2026-09-12 09:16:31 +0530

    input given by user(division).c

---
+++ Lab_02/input given by user(division).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+    int a;
+    int b;
+    printf("Enter a\n");
+    scanf("%d",&a);
+    printf("Enter b\n");
+    scanf("%d", &b);
+    printf("Division of a and b is %d", a/b);
+    return 0;
+
+}
