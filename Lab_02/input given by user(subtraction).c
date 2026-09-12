b1b588705d05d3dca06740441fdb397e9ed07d48
Vijay-Choudhary1 <vc587587@gmail.com>
2026-09-12 09:07:13 +0530

    input given by user(subtraction).c

---
+++ Lab_02/input given by user(subtraction).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+    int a;
+    int b;
+    printf("Enter a\n");
+    scanf("%d",&a);
+    printf("Enter b\n");
+    scanf("%d", &b);
+    printf("Difference of a and b is %d", a-b);
+    return 0;
+
+}
