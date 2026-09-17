bf75108da432eb25891e34e20eb5efa8bf3ca966
Vijay-Choudhary1 <vc587587@gmail.com>
2026-09-17 08:09:03 +0530

    Create Error detection ques4.c

---
+++ Lab_04/Error detection ques4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int 1number = 10;
+    printf("%d", 1number);
+    return 0;
+}
+
+// errro - name ofint should can start with number can only be start with _ and alphabet
