796936cf2cde0fbe92093e9aa7df81dc5bcd87ec
Vijay-Choudhary1 <vc587587@gmail.com>
2026-09-26 08:21:32 +0530

    Create code.c

---
+++ Lab_03/Relational Operator/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a=23;
+    int b=14;
+    printf("%d\n", a==b);
+    printf("%d\n", a>b);
+    printf("%d\n", a<b);
+    printf("%d\n", a<=b);
+    printf("%d\n", a>=b);
+    printf("%d\n", a!=b);
+    return 0;
+
+}
